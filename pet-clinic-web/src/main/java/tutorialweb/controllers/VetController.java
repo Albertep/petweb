500a137871a6e0047262fe761f2a4113e78016e8
Albertep <alberte9494@gmail.com>
2020-10-20 19:24:39 +0200

    El index de los pveterinarios Vets Ya lista los Veterinarios de la memoria. Closes #15.

--- pet-clinic-web/src/main/java/tutorialweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/tutorialweb/controllers/VetController.java
@@ -1,14 +1,23 @@
 package tutorialweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tutorialweb.petweb.services.VetService;
+import tutorialweb.petweb.services.map.VetServiceMap;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vet/index","vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vet/index","vets/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
