78ee291343905e67d27fb9661fea934baa4f6ea4
Albertep <alberte9494@gmail.com>
2020-10-22 12:27:58 +0200

    Added Specialty POJO and Specialty to Vet. Closes #2.

---
+++ pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Specialty.java
@@ -0,0 +1,14 @@
+package tutorialweb.petweb.Model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
