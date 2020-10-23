3685abc9b91d9929ab1c9585cf7d40b40c087bd7
Albertep <alberte9494@gmail.com>
2020-10-23 09:25:53 +0200

    Added Speciality Map and Interface . Closes #31.

--- pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Speciality.java
+++ pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Speciality.java
@@ -1,6 +1,6 @@
 package tutorialweb.petweb.Model;
 
-public class Specialty extends BaseEntity {
+public class Speciality extends BaseEntity {
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/tutorialweb/petweb/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package tutorialweb.petweb.services;
+
+import tutorialweb.petweb.Model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
