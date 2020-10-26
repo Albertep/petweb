9fdc0ac3ec4bdd39d411be88bb220c5dbf8edb69
Albertep <alberte9494@gmail.com>
2020-10-26 08:45:00 +0100

    Update rest of the Entities . Closes #37.

--- pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Speciality.java
+++ pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Speciality.java
@@ -1,7 +1,15 @@
 package tutorialweb.petweb.Model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Specialties")
 public class Speciality extends BaseEntity {
 
+    @Column("description")
     private String description;
 
     public String getDescription() {
