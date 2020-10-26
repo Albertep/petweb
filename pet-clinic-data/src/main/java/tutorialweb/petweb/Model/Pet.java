3d8551f374e434c9e3a9d92c35585a894d142b6b
Albertep <alberte9494@gmail.com>
2020-10-26 08:53:52 +0100

    Visit mapping . Closes #38.

--- pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Pet.java
+++ pet-clinic-data/src/main/java/tutorialweb/petweb/Model/Pet.java
@@ -2,6 +2,8 @@ package tutorialweb.petweb.Model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birthday")
     private LocalDate date;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit>visits=new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
